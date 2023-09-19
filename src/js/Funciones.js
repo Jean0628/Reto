e96e02847086eca6884b532f14efa2ffd49fb914
MDMS29 <mdmazo30@misena.edu.co>
2023-09-19 15:25:18 -0500

    cambio

--- src/js/Funciones.js
+++ src/js/Funciones.js
@@ -66,40 +66,42 @@
         //     movimientosSpan.textContent = movimientos;
         // }
 // JS 
-// parte lógica
+// Declarar variables globales
+let iconos = [];
+let selecciones = [];
 let aciertos = 0;
 let movimientos = 0;
 let tiempoInicio;
 let tiempoInterval;
 
-    let iconos = []
-    let selecciones = []
-
-    generarTablero()
 
     function cargarIconos() {
         iconos = [
-            '<img src="https://i.ibb.co/pK31kwZ/00.png" width="100%">',
+            '<img src="https://i.ibb.co/pK31kwZ/00.png" width="70%">',
             '<img src="https://i.ibb.co/4SG5shk/580b57fbd9996e24bc43c02c.png" width="100%">',
-            '<img src="https://i.ibb.co/GVLFSxH/00.png" width="100%">',
-            '<img src="https://i.ibb.co/SmVF5vB/00.png" width="100%">',
-            '<img src="https://i.ibb.co/y6z683V/00.png" width="100%">',
+            '<img src="https://i.ibb.co/GVLFSxH/00.png" width="70%">',
+            '<img src="https://i.ibb.co/SmVF5vB/00.png" width="70%">',
+            '<img src="https://i.ibb.co/y6z683V/00.png" width="70%">',
             '<img src="https://i.ibb.co/85r8z0f/9978a389246e31ed58bf4ebfc0cbe7ec.png" width="100%">',
             '<img src="https://i.ibb.co/mC2pqc5/00.jpg" width="70%">',
             '<img src="https://i.ibb.co/JQBmBXN/00.png" width="70%">',
-            '<img src="https://i.ibb.co/z5ZgQwN/00.png" width="100%">',
+            '<img src="https://i.ibb.co/z5ZgQwN/00.png" width="70%">',
             '<img src="https://i.ibb.co/dJZgXW2/image-snorlaxpng-pokemon-shuffle-wiki-fandom-powered-by-wikia-snorlax-png-256-256.png" width="100%">',
-            '<img src="https://i.ibb.co/LJTbjXd/00.gif" width="100%">',
-            '<img src="https://i.ibb.co/JHLQTz8/00.gif" width="100%">',
+            '<img src="https://i.ibb.co/LJTbjXd/00.gif" width="70%">',
+            '<img src="https://i.ibb.co/JHLQTz8/00.gif" width="70%">',
         ]
     }
 
     function generarTablero() {
-        cargarIconos()
-        let len = iconos.length
-        selecciones = []
-        let tablero = document.getElementById("tablero")
-        let tarjetas = []
+        cargarIconos();
+        let len = iconos.length;
+        selecciones = [];
+        aciertos = 0; // Reiniciar aciertos
+        movimientos = 0; // Reiniciar movimientos
+        actualizarEstadisticas(); // Actualizar las estadísticas en el HTML
+        iniciarCronometro(); // Iniciar el cronómetro
+        let tablero = document.getElementById("tablero");
+        let tarjetas = [];
         
         for (let i = 0; i < len*2; i++) {
             tarjetas.push(`
@@ -149,3 +151,42 @@ let tiempoInterval;
             }
         }, 1000);
     }
+
+    function actualizarEstadisticas() {
+        document.getElementById("C_aciertos").textContent = aciertos;
+        document.getElementById("C_movimientos").textContent = movimientos;
+    }
+    function iniciarCronometro() {
+        tiempoInicio = new Date().getTime();
+        tiempoInterval = setInterval(actualizarTiempo, 1000);
+    }
+    
+    function actualizarTiempo() {
+        const tiempoActual = new Date().getTime();
+        const tiempoTranscurrido = (tiempoActual - tiempoInicio) / 1000;
+        document.getElementById("Cronometro").textContent = tiempoTranscurrido.toFixed(0);
+    }
+
+            function cronometro() {
+            desbloquearCards();
+            let time = 5; // Cambié el tiempo a 60 segundos
+            startButtons.forEach(startButton => {
+                startButton.disabled = true; // Deshabilita los botones de inicio una vez que se inicia el juego
+            });
+            const contador = setInterval(() => {
+                time--;
+                Cronometro.innerHTML = time;
+                if (time === 0) {
+                    clearInterval(contador);
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: '¡Se te acabó el tiempo!',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            location.reload();
+                        }
+                    });
+                }
+            }, 1000);
+        }
