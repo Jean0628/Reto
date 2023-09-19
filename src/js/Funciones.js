9240563bea9b5d27ff7199792a869a0538dbeb95
MDMS29 <mdmazo30@misena.edu.co>
2023-09-19 14:48:53 -0500

    reto

--- src/js/Funciones.js
+++ src/js/Funciones.js
@@ -1,68 +1,83 @@
-//Variables
+// //Variables
 
-const card = document.querySelectorAll('.card');
-const start = document.querySelectorAll('.start');
-const Cronometro = document.getElementById('Cronometro');
+// const card = document.querySelectorAll('.card');
+// const start = document.querySelectorAll('.start');
+// const Cronometro = document.getElementById('Cronometro');
 
-//Eventos 
-document.addEventListener('DOMContesntLoades', ()=>{
-    inicar();
-})
+// //Eventos 
+// document.addEventListener('DOMContesntLoades', ()=>{
+//     inicar();
+// })
 
-start.forEach(start => {
-    start.addEventListener('click', () => {
-        cronometro();
-    });
-});
-//fin de los eventos
+// start.forEach(start => {
+//     start.addEventListener('click', () => {
+//         cronometro();
+//     });
+// });
+// //fin de los eventos
 
 
-//Funcion 
-function iniciar(){
-    for (let i = 0; i < card.length; i++){
-        card[i].disabled = true;
-    }
-}
+// //Funcion 
+// function iniciar(){
+//     for (let i = 0; i < card.length; i++){
+//         card[i].disabled = true;
+//     }
+// }
 
-function desbloquearCards(){
-    for (let i = 0; i < card.length; i++){
-        card[i].disabled = false;
-    }
-}
+// function desbloquearCards(){
+//     for (let i = 0; i < card.length; i++){
+//         card[i].disabled = false;
+//     }
+// }
 
-function cronometro(){
-    desbloquearCards();
-    let time = 5;
-    start.forEach(start => {
-        start.classList.add('disable');
-    });
-    const contador = setInterval(()=> {
-        time--
-        Cronometro.innerHTML = time;
-        if (time == 0){
-            clearInterval(contador);
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: '¡Se te acabó el tiempo!',
-            })
-            .then((result) =>{
-                if(result.isConfirmed){ // Corregido "isConfirmed"
-                    location.reload();
-                }
-            })
-        }
-    }, 1000);
-}
+// function cronometro(){
+//     desbloquearCards();
+//     let time = 60;
+//     start.forEach(start => {
+//         start.classList.add('disable');
+//     });
+//     const contador = setInterval(()=> {
+//         time--
+//         Cronometro.innerHTML = time;
+//         if (time == 0){
+//             clearInterval(contador);
+//             Swal.fire({
+//                 icon: 'error',
+//                 title: 'Oops...',
+//                 text: '¡Se te acabó el tiempo!',
+//             })
+//             .then((result) =>{
+//                 if(result.isConfirmed){ // Corregido "isConfirmed"
+//                     location.reload();
+//                 }
+//             })
+//         }
+//     }, 1000);
+// }
 
-function mostrarImagen(button) {
-    // Obtener el atributo "data-image" del botón
-    const imageUrl = button.getAttribute('data-image');
+//variables de cartas
+// let tarjetaDestapadas = 0;
+// let tarjetas1 = null;
+// let tarjetas2 = null;
+// let primerResultado = null;
+// let segundoResultado = null;
 
-    // Mostrar la imagen en un cuadro de diálogo o donde desees
-    Swal.fire({
-        imageUrl: imageUrl,
-        imageAlt: 'src/assets/IMG/computador.jpg',
-        showConfirmButton: false
-    });
+
+let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+numeros = numeros.sort(()=>{return Math.random() -0.5});
+console.log(numeros);
+
+
+function destapar(id){
+    // tarjetaDestapadas++;
+    // console.log(tarjetaDestapadas);
+
+    // if(tarjetaDestapadas == 1){
+    //     tarjetas1 = document.getElementById(id);
+    //     primerResultado = numeros[id]
+    //     tarjetas1.innerHTML = primerResultado;
+
+    //     //deshabilita primer boton
+    //     tarjetas1.disabled = true;
+// }
 }
