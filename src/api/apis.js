8886cd18271175f0d4949ed30b13cb3ef9b53695
MDMS29 <mdmazo30@misena.edu.co>
2023-09-19 16:30:29 -0500

    cambio

--- src/api/apis.js
+++ src/api/apis.js
@@ -1,8 +1,34 @@
 console.log('hola')
 const data = {
-    email: "usuario12@gmai.com",
+    email: "usuario12@gmail.com",
     password: "kuberausuario12"
 }
 
-axios.get('http://sena.kubera.work/api/v3/auth/sign_in', data)
-.then(( response ) => console.log( response ))
+const headersLocal = JSON.parse(localStorage.getItem('headers')) || {};
+
+async function peticionLogin() {
+    const result = await axios.post('http://sena.kubera.work/api/v3/auth/sign_in', data, { mode: "cors" })
+    .then(( response ) => { return response})
+
+    const headers = {
+        'access-token': result.headers['access-token'],
+        'uid': result.headers['uid'],
+        'client': result.headers['client'],
+        'expired': result.headers['expires'],
+      };
+
+      localStorage.setItem('headers',JSON.stringify(headers))
+}
+peticionLogin()
+
+
+
+//Listado
+
+async function peticionListado() {
+    const result = await axios.get('http://sena.kubera.work/api/v3/brands', {headers : headersLocal  })
+    .then(( response ) => { return response})
+    
+    console.log(result);
+}
+peticionListado()
