07e5c8d6b1063f8350985a5706fd4cb6c8c8bd6f
MDMS29 <mdmazo30@misena.edu.co>
2023-09-19 11:40:26 -0500

    reto

--- src/js/Funciones.js
+++ src/js/Funciones.js
@@ -1,3 +1,32 @@
 //Variables
 
 const card = document.querySelectorAll('.card');
+const start = document.querySelectorAll('.start');
+
+//Eventos 
+document.addEventListener('DOMContesntLoades', ()=>{
+    inicar();
+})
+
+start.addEventListener('click', () =>{
+
+})
+//fin de los eventos
+
+
+//Funcion 
+function iniciar(){
+    for (let i = 0; i < card.length; i++){
+        card[i].disabled = true;
+    }
+}
+
+function desbloquearCards(){
+    for (let i = 0; i < card.length; i++){
+        card[i].disabled = false;
+    }
+}
+
+function cronometro(){
+    desbloquearCards()
+}
